8530959f9a6b1b7ed3342dfa2ce010f308189cbe
Clinton Medbery <clintomed@gmail.com>
2021-01-12 21:45:20 -0500

    Updated look

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,27 +1,53 @@
-import React from 'react'
-import styles from './HomePageStyles.module.css'
-import {Card} from "antd";
+import React from "react";
+import { Card } from "antd";
 import Main from "../Main/Main";
 
 const HomePage = () => {
   return (
     <Main>
       <div>
-        <div style={{marginBottom: '1em'}}>
-          Hello my name is Clint and this is my web page. There is crap all here but we are just getting started.
+        <div style={{ marginBottom: "1em" }}>
+          Hello my name is Clint and this is my web page. I work at
         </div>
         <h3>A few projects of mine...</h3>
-        <Card title={"yabalog"}>
+        <Card title={"yabalog"} style={{ marginBottom: "1em" }}>
           <div>
-            Electron app that gathers logs recursively in a local folder or gathers them from kubectl. Search local logs or connect to the cloud.
+            Electron app that gathers logs recursively in a local folder or
+            gathers them from kubectl. Search local logs or connect to the
+            cloud.
           </div>
           <a href="https://github.com/clintonmedbery/yabalog" target="_blank">
             yabalog - GitHub
           </a>
         </Card>
+        <Card title={"FreeAAC"} style={{ marginBottom: "1em" }}>
+          <div>
+            Open Source React Native tablet application aimed at helping people
+            who have trouble speaking communicate.
+          </div>
+          <a
+            href="https://www.assistiveware.com/learn-aac/what-is-aac"
+            target="_blank"
+          >
+            What is AAC?
+          </a>
+          <br />
+          <a href="https://github.com/clintonmedbery/FreeAAC" target="_blank">
+            FreeAAC - GitHub
+          </a>
+        </Card>
+        <Card title={"This site"}>
+          <div>clintonmedbery.dev written in React using Ant Design.</div>
+          <a
+            href="https://github.com/clintonmedbery/clinton-dev-site"
+            target="_blank"
+          >
+            clinton-dev-site - GitHub
+          </a>
+        </Card>
       </div>
     </Main>
-  )
+  );
 };
 
 export default HomePage;
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,64 +1,53 @@
-import React, {useState} from 'react'
-import {Avatar, Layout, Menu, Row,} from "antd";
-import Icon from '@ant-design/icons';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHome, faDesktop, faHdd} from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import { Avatar, Layout, Menu, Row } from "antd";
+import Icon from "@ant-design/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faDesktop, faHdd } from "@fortawesome/free-solid-svg-icons";
 import HomePage from "../HomePage/HomePage";
-import {Link} from "react-router-dom";
-import MeLogo from '../../Images/Avatar.jpeg'
-import styles from './Main.module.css'
+import { Link } from "react-router-dom";
+import MeLogo from "../../Images/Avatar.jpeg";
+import styles from "./Main.module.css";
 
 const { Header, Content, Sider } = Layout;
 
 const Main = (props) => {
-  let [sideCollapsed, setSideCollapsed] = useState(false)
+  let [sideCollapsed, setSideCollapsed] = useState(false);
+
   return (
     <Layout>
       <Header>
-        <div className={styles.header} >
-            <Avatar src={MeLogo}/>
-            <div className={styles.text}>
-              ClintonMedbery.dev
-            </div>
-        </div>
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item key="1" style={{ marginTop: "-.2em" }}>
+            <Link to="/">
+              <div className={styles.header}>
+                <Avatar src={MeLogo} />
+                <div className={styles.text}>ClintonMedbery.dev</div>
+              </div>
+              <div className="logo" />
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/desktop">
+              <Icon>
+                <FontAwesomeIcon icon={faDesktop} fixedWidth />
+              </Icon>
+              <span>Desktop</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <a href="http://www.clintonmedbery.com/" target="_blank">
+              <Icon>
+                <FontAwesomeIcon icon={faHdd} fixedWidth />
+              </Icon>
+              <span>My Old Stuff</span>
+            </a>
+          </Menu.Item>
+        </Menu>
       </Header>
-      <Layout>
-        <Sider trigger={null} collapsible collapsed={sideCollapsed}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline">
-            <Menu.Item key="1" style={{marginTop: '-.2em'}}>
-              <Link to="/">
-                <Icon>
-                  <FontAwesomeIcon icon={faHome} fixedWidth />
-                </Icon>
-                <span >Home Page</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Link to="/desktop">
-                <Icon>
-                  <FontAwesomeIcon icon={faDesktop} fixedWidth />
-                </Icon>
-                <span>Desktop</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <a href="http://www.clintonmedbery.com/" target="_blank">
-                <Icon>
-                  <FontAwesomeIcon icon={faHdd} fixedWidth />
-                </Icon>
-                <span>My Old Stuff</span>
-              </a>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Content>
-          <div className={styles.mainContainer}>
-            {props.children}
-          </div>
-        </Content>
-      </Layout>
+      <Content>
+        <div className={styles.mainContainer}>{props.children}</div>
+      </Content>
     </Layout>
-  )
+  );
 };
-export default Main
+export default Main;
